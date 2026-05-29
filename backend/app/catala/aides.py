961d593bf27cd8f46095289df2339907fe1a0ae3
edelagnier <erica.delagnier@multi.coop>
2026-05-29 11:44:22 +0200

    test all relative import

--- backend/app/catala/aides.py
+++ backend/app/catala/aides.py
@@ -1,5 +1,5 @@
-from catala.generated.Personne import Personne
-from catala.generated.catala_runtime import Money, Integer
+from .generated.Personne import Personne
+from .generated.catala_runtime import Money, Integer
 
 def impot_revenu(montant) -> str:
     personne = Personne(Money(Integer(100000)), Integer(3))
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 from .utils import logger
-from config import setup_sentry
-from api import default
+from .config import setup_sentry
+from .api import default
 
 setup_sentry()
 app = FastAPI()
