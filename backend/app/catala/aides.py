49d92631c302e9dfa99bc57b5483346c0ec01961
edelagnier <erica.delagnier@multi.coop>
2026-05-29 10:40:54 +0200

    ✨ - v0.7 - feat - replace catala package by generated module

--- backend/app/catala/aides.py
+++ backend/app/catala/aides.py
@@ -1,5 +1,5 @@
-from prestagri_catala.Personne import Personne
-from prestagri_catala.catala_runtime import Money, Integer
+from catala.generated.Personne import Personne
+from catala.generated.catala_runtime import Money, Integer
 
 def impot_revenu(montant) -> str:
     personne = Personne(Money(Integer(100000)), Integer(3))
