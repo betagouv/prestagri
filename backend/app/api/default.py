2c10b446f174dd28eb3f6b6b93ea3708b72a795a
edelagnier <erica.delagnier@multi.coop>
2026-06-08 14:44:46 +0200

    ✨ - v0.12 - feat - compute points aide scolarite

--- backend/app/api/default.py
+++ backend/app/api/default.py
@@ -1,9 +1,8 @@
 from pydantic import BaseModel
 from fastapi import APIRouter
 from ..utils import logger
-from ..catala.aides import quotient_familial, quotient_familial_aide_scolarite
-from ..model.famille import Famille
-from ..model.personne import Personne
+from ..catala.aides import quotient_familial, quotient_familial_aide_scolarite, criteres_eligibles_aide_scolarite
+from ..model import Famille, Personne, Trajet, Response
 
 
 router = APIRouter()
@@ -12,10 +11,6 @@ router = APIRouter()
 def read_root():
     logger.info('Bienvenu')
     return {"Quelle belle journee pour les abeilles"}
-
-class Response(BaseModel):
-    value: str
-    explanation: str 
 
 @router.get("/quotient_familial")
 def read_quotient_familial(
@@ -29,7 +24,7 @@ def read_quotient_familial(
     outre_mer: bool = False
     ) -> Response :
     famille = create_famille(agent_revenu, agent_enfants, conjoint_revenu, conjoint_enfants, personne_ou_enfant_porteur_handicap, garde_alternee, parent_isole, outre_mer)
-    return Response(value = quotient_familial(famille), explanation = "not yet available")
+    return quotient_familial(famille)
 
 
 @router.get("/aide_scolarite/quotient_familial")
@@ -44,10 +39,24 @@ def read_quotient_familial_aide_scolarite(
     garde_alternee: bool = False,
     parent_isole: bool = False,
     outre_mer: bool = False
-    ):
+    ) -> Response:
     etudiant_independant = Personne(revenu=etudiant_revenu, enfants=etudiant_enfants)
     famille = create_famille(agent_revenu, agent_enfants, conjoint_revenu, conjoint_enfants, personne_ou_enfant_porteur_handicap, garde_alternee, parent_isole, outre_mer)
-    return Response(value = quotient_familial_aide_scolarite(famille, [etudiant_independant]), explanation = "not yet available")
+    return quotient_familial_aide_scolarite(famille, [etudiant_independant])
+
+@router.get("/aide_scolarite/points")
+def read_quotient_familial_aide_scolarite(
+    adresse_agent: str,
+    adresse_etablissement: str,
+    adresse_etudiant: str | None = None,
+    montant_materiel_specifique: int | None = None,
+    etudiant_post_bac: bool = False,
+    ) -> Response :
+    trajet_agent = Trajet(distance_km=40, duree_minutes=40)
+    trajet_etudiant = Trajet(distance_km=30, duree_minutes=30)
+    valeur_point = 30
+    return criteres_eligibles_aide_scolarite(trajet_agent, trajet_etudiant, montant_materiel_specifique, valeur_point, etudiant_post_bac)
+
 
 @router.get("/error-simulator")
 async def trigger_error():
